e34c13575d7b605a9f337f1b879cbe31c7192cee
Prab <prabnurbal@gmail.com>
2020-10-19 00:09:51 +0530

    Reduced lower margin for background image

--- src/components/background-image-full.js
+++ src/components/background-image-full.js
@@ -3,7 +3,7 @@ import BackgroundImage from 'gatsby-background-image';
 import tw from 'twin.macro';
 
 const BackgroundImageFull = ({ children, fluid, fixed, title }) => (
-  <div tw="w-full h-screen flex overflow-hidden mb-24 md:mb-32 xl:mb-40">
+  <div tw="w-full h-screen flex overflow-hidden mb-16 md:mb-20 xl:mb-24">
     <BackgroundImage
       fluid={fluid}
       fixed={fixed}
