dd2a0d32daf76239718996b1986b3429b8ce474c
Prab <prabnurbal@gmail.com>
2020-10-18 22:57:23 +0530

    Captain Amarinder Singh picture added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,39 @@
 import React, { useContext } from 'react'
 import { graphql, useStaticQuery } from 'gatsby';
-
+import BackgroundImage from 'gatsby-background-image';
 import tw from 'twin.macro';
 
 import { GetText } from '../utils/text/textProvider';
 import Header from '../components/header';
 import BackgroundImageFull from '../components/background-image-full';
 
-// const Test = () =>
-// <div tw='bg-gray-400 flex-auto items-start'>
-//   <span tw='bg-indigo-700 w-12 h-12 m-3 block'/>
-//   <span tw='bg-teal-700 w-full h-16 m-3 block float-right'/>
-//   <span tw='bg-indigo-700 w-10 h-10 m-3 block'/>
-// </div>
-
-// const TextPiece = tw.h2`block my-12`;
+// const Image = tw(BackgroundImage)`
+//   rounded md:w-10/12 lg:w-9/12 xl:w-8/12 
+//   bg-cover bg-center
+//   mx-4 sm:mx-8 md:mx-4 lg:mx-8
+// `;
+const StyledImage = tw(BackgroundImage)`mt-0 m-auto rounded border-8 border-red-700`;
+const Image = tw(BackgroundImage)`
+  rounded md:w-11/12 lg:w-10/12 xl:w-9/12 md:h-144
+   bg-cover bg-center
+  mx-auto
+`;
 
 const AboutMe = () => {
   const getText = useContext(GetText);
   const text = getText('aboutMe')
 
-  const imgData = useStaticQuery(graphql`
+  const { bg, am } = useStaticQuery(graphql`
     query {
-      file(relativePath: { eq: "aboutMeCropped.jpg" }) {
+      bg: file(relativePath: { eq: "aboutMeCropped.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1920, maxHeight: 1080, quality: 90) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
+
+      am: file(relativePath: { eq: "amarinder-cropped-edited.png" }) {
         childImageSharp {
           fluid(maxWidth: 1920, maxHeight: 1080, quality: 90) {
             ...GatsbyImageSharpFluid_withWebp_tracedSVG
@@ -33,18 +44,25 @@ const AboutMe = () => {
   `)
 
   return <>
-    <BackgroundImageFull fluid={imgData.file.childImageSharp.fluid} title="Paramjit Singh Bal">
+    <BackgroundImageFull fluid={bg.childImageSharp.fluid} title="Paramjit Singh Bal">
       <Header section='AM'/>
     </BackgroundImageFull>
     
     <div tw='flex-auto mt-24'>
       <h2 tw="block my-16 text-center font-bold">{text['greeting']}</h2>
-      {text['intro'].map((item) => 
-        <p tw="block mt-12 w-10/12 mx-auto text-3xl">
+      {text['intro'].map((item, idx) => 
+        <p key={idx} tw="block mt-12 w-10/12 mx-auto text-3xl">
           {item}
         </p>
       )}
     </div>
+
+    
+      {/* <div tw="space-x-64"/> */}
+    <div tw="justify-center items-center pb-20">
+      <Image fluid={am.childImageSharp.fluid}/>
+    </div>  
+    
   </>;
 }
 
